a2737fdc02ebf285b725ca87fc633dd316f963bc
Sergey <SergeyGaponov@Mac-mini-Yuriy.local>
2017-12-05 17:47:48 +0200

    Initial Commit

---
+++ Multithreading_hw18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multithreading_hw18
+//
+//  Created by Sergey Intern  on 12/5/17.
+//  Copyright © 2017 Sergey Intern . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
